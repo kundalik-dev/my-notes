bc955cfecaf2c3e6b7da1f17bf543f934eaf0937
kundalik-dev <kundalik-dev@gmail.com>
2026-06-10 10:57:02 +0530

    feat(sql): Completed upto 22 page number.

--- sql/Sql Scripts/01-Bankdb-daily-scripts.sql
+++ sql/Sql Scripts/01-Bankdb-daily-scripts.sql
@@ -32,9 +32,19 @@ from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'Loans';
 -- NOT BETWEEN
 -- IN
 -- NOT IN
-
--- IN
+ 
 select * from customers where customerid in(1,10);
 select * from Customers where CustomerID between 1 AND 10;
 
- 
+--=============================Practice 10/June/2026 ===================================================
+-- INSERT INTO => using column name with values 
+--             => without column names with values only
+-- UPDATE => remember to use where clause otherwise it will update all the records in the table
+-- DELETE => Single Delete OR Full Table data delete by keeping table schema
+--        => Remember to use where clause otherwise it will delete all the records in the table
+-- SELECT INTO => Create a new table and insert the data into it from another table
+-- ALIAS => Column Alias, Table Alias
+-- UNION => Slow, combine unique records from two or more select statements.
+-- UNION ALL => Fast, combine all records from two or more select statements including duplicates.
+--        => Remember CurrentCustomer and ArchiveCustomer tables example
+-- DROP => Delete entire table
